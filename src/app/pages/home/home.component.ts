c61605c35e9f379d76f1784a0790cdc8f12e39b5
Francisco Lopez <franlopbri@gmail.com>
2020-10-11 16:57:46 -0500

    se muestran datos de videos en home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import { Video } from 'src/app/models/youtube.models';
 import { YoutubeService } from 'src/app/services/youtube.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
+  styleUrls: [
+    './home.component.css'
   ]
 })
 export class HomeComponent {
 
+  videos: Video[] = [];
   constructor(private youtube: YoutubeService) { 
     this.youtube.getVideos().subscribe(data => {
+      this.videos.push(...data);
       console.log(data);
     })
   }
